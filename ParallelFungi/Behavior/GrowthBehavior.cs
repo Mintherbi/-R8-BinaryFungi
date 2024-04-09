20f03b146312865157aee4c49e670d3682731bb2
jord97@korea.edu <jord97@korea.ac.kr>
2024-04-09 16:47:00 +0900

    Change Program Construction

---
+++ ParallelFungi/Behavior/GrowthBehavior.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace ParallelFungi.Behavior
+{
+    public class GrowthBehavior : GH_Component
+    {
+        /// <summary>
+        /// Initializes a new instance of the MyComponent1 class.
+        /// </summary>
+        public GrowthBehavior()
+          : base("GrowthBehavior", "GB",
+              "Set Growth Property of Fungi",
+              "BinaryNature", "BinaryFungi")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddIntegerParameter("Growth_Rate", "GR", "Growing Length per time", GH_ParamAccess.item, 1);
+            pManager.AddNumberParameter("Branch_Rate", "BR", "Growing Length per time", GH_ParamAccess.item,0.1);
+            pManager.AddNumberParameter("Neighbor_Sensing_Rate", "NR", "Growing Length per time", GH_ParamAccess.item,3);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Growth_Behavior","GB",)
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object is used to retrieve from inputs and store in outputs.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+        }
+
+        /// <summary>
+        /// Provides an Icon for the component.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                //You can add image files to your project resources and access them like this:
+                // return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Gets the unique ID for this component. Do not change this ID after release.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("FB8592F0-FCB9-43C2-B9C0-13429036E15C"); }
+        }
+    }
+}
---
+++ ParallelFungi/Data/GrowthData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParallelFungi.Data
+{
+    public struct GrowthData
+    {
+        public int r_growth { get; set; }
+        public double r_branch { get; set; }
+        public double coef_neighbor_sensing { get; set; }
+
+        public GrowthData(int r_growth = 1, double r_branch = 0.1, double coef_neighbor_sensing = 3)
+        {
+            this.r_growth = r_growth;
+            this.r_branch = r_branch;
+            this.coef_neighbor_sensing = coef_neighbor_sensing;
+        }
+    }
+}
